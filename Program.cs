742818c4b3fe08651fe3785cb42cff841d6a15bd
David Lucas <davidlucaskl@gmail.com>
2023-06-09 08:58:50 -0300

    Criando a Primeira Classe

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using tabuleiro;
+
+
+    class Program {
+    static void Main(string[] args) {
+
+        Posicao P;
+        P = new Posicao(3, 4);
+        Console.WriteLine("Posição : " + P);
+
+
+
+
+
+
+
+       
+    }
+}
