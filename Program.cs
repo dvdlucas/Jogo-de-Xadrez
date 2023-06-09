bfc955fd7509eb0ee6e9f6331dcbe8b09d75d7ed
David Lucas <davidlucaskl@gmail.com>
2023-06-09 09:18:28 -0300

    Implementando Novas classes no tabuleiro

--- Program.cs
+++ Program.cs
@@ -5,9 +5,7 @@ using tabuleiro;
     class Program {
     static void Main(string[] args) {
 
-        Posicao P;
-        P = new Posicao(3, 4);
-        Console.WriteLine("Posição : " + P);
+        Tabuleiro tab = new Tabuleiro(8, 8);
 
 
 
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace tabuleiro { 
+class Peca{
+
+		public Posicao posicao { get; set; }
+		public Cor cor { get; set; }
+		public int qteMovimento { get; protected set; }
+		public Tabuleiro tab { get; protected set; }
+
+		public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+		{
+			this.posicao = posicao;
+			this.tab = tab;
+			this.cor = cor;
+			this.qteMovimento = 0;
+		}
+
+
+
+
+
+	}
+	
+}
