5f68d4bcfb9e9eb8356a79c23d5ab79adfa72077
David Lucas <davidlucaskl@gmail.com>
2023-06-15 13:30:15 -0300

    Jogada Promoção, Finalizado Processo

--- xadrez/PartidaXadrez.cs
+++ xadrez/PartidaXadrez.cs
@@ -154,6 +154,22 @@ namespace xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroExceptions("Voce não pode se colocar em Xeque !!!");
             }
+
+            Peca p = tab.peca(destino);
+
+            // Jogada Expecial Promoção
+            if (p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0 ) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversario(jogadorAtual)))
             {
                 xeque = true;
@@ -171,7 +187,12 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-            Peca p = tab.peca(destino);
+            
+
+            
+
+
+      
 
             //#Jogada Especial en passant
             if(p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
